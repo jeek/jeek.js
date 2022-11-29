ddabdae6218f826557460ab81712f3f7b635e61e
T.J. Eckman <jeek@jeek.net>
2022-11-29 02:00:03 +0000

    Autobuild

--- jeek.js
+++ jeek.js
@@ -4630,3 +4630,6 @@ export async function main(ns) {
   }
   ns.tprint(data.pop());
 } */
+
+// https://discord.com/channels/415207508303544321/944647347625930762/1046962547582058496
+// (()=>{let times=[],fn=(off)=>{if (times.length >= 100) {if (times.length == 100) {console.log(times.join("\n"))}; return}; let n=Date.now(); let m=n%4==off?4:8-(n-off)%4; times.push(off+","+m+","+n); setTimeout(fn, m, off)};[0,1,2,3].forEach(fn)})()
