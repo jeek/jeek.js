56d60878021b298909bc66f97abc9ccbd5e15984
T.J. Eckman <jeek@jeek.net>
2023-01-22 23:53:18 -0500

    Attempt #7

--- Windows.js
+++ Windows.js
@@ -1,14 +1,20 @@
 // Thanks to omuretsu
 let slp = ms => new Promise(r => setTimeout(r, ms));
 export let makeNewWindow = async (title = "Default Window Title", theme) => {
-	let win = open("main.bundle.js", title.replaceAll(" ", "_"), "popup=yes,height=200,width=500,left=100,top=100,resizable=yes,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no");
+  let win = open("main.bundle.js", title.replaceAll(" ", "_"), "popup=yes,height=200,width=500,left=100,top=100,resizable=yes,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no");
   let good = false;
+  let doc = 0;
   while (!good) {
-    good = true;
     await slp(1000);
     try {
-    let doc = win["document"];
-	doc.head.innerHTML = `
+      doc = win["document"];
+      doc.head.innerHTML = "No.";
+      good = true;
+    } catch {
+      good = false;
+    }
+  }
+  doc.head.innerHTML = `
   <title>${title}</title>
   <style>
     *{
@@ -69,14 +75,10 @@ export let makeNewWindow = async (title = "Default Window Title", theme) => {
     }
   </style>`;
   doc.body.innerHTML = `<div class=title>${title}</div><div class=logs><p></p></div>`;
-	let logs = doc.body.querySelector(".logs");
-  } catch {
-    good = false;
+  let logs = doc.body.querySelector(".logs");
+  win.update = (content) => {
+    logs.innerHTML = content;
   }
-  }
-	win.update = (content) => {
-		logs.innerHTML = content;
-	}
-	win.reopen = () => open("steam_appid.txt", title.replaceAll(" ", "_"), "popup=yes,height=200,width=500,left=100,top=100,resizable=yes,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no");
-	return win;
+  win.reopen = () => open("main.bundle.js", title.replaceAll(" ", "_"), "popup=yes,height=200,width=500,left=100,top=100,resizable=yes,scrollbars=no,toolbar=no,menubar=no,location=no,directories=no,status=no");
+  return win;
 }
