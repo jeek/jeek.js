e40cb2c39e4485fdc5e2f99368dbf9dd7a4ba9fe
T.J. Eckman <jeek@jeek.net>
2023-02-16 08:00:02 +0000

    Autobuild

--- jeek.js
+++ jeek.js
@@ -739,11 +739,11 @@ export async function bn8(Game) {
     while ((!await Do(Game.ns, "ns.stock.has4SDataTIXAPI", ""))) {
         if ((await (Game.StockMarket.portfolioValue)) + (await Do(Game.ns, "ns.getPlayer")).money > 25000000000 * ((await Do(Game.ns, "ns.getBitNodeMultipliers"))).FourSigmaMarketDataApiCost) {
             Game.ns.write('/temp/4s.js', "export async function main(ns) { for (let stock of ns.stock.getSymbols()) { ns.stock.getPosition(stock)[0] ? ns.stock.sellStock(stock, ns.stock.getPosition(stock)[0]) : 0; ns.stock.getPosition(stock)[2] ? ns.stock.sellShort(stock, ns.stock.getPosition(stock)[2]) : 0; } ns.stock.purchase4SMarketDataTixApi(); }",'w')
-        };
-        await Game.ns.asleep(0);
-        Game.ns.run('/temp/4s.js');
-        while (tickPrice == await Do(Game.ns, "ns.stock.getPurchaseCost", 'ECP', 1, "Long")) {
             await Game.ns.asleep(0);
+            Game.ns.run('/temp/4s.js');
+        };
+        while (tickPrice == await Do(Game.ns, "ns.stock.getPurchaseCost", 'ECP', 1, "Long")) {
+            await Game.ns.asleep(1000);
         }
         tickPrice = await Do(Game.ns, "ns.stock.getPurchaseCost", 'ECP', 1, "Long");
         prices.push({});
