f94e64cf7b6b4671bfd7ab6e0a7bf215bb90e949
T.J. Eckman <jeek@jeek.net>
2023-02-19 05:00:01 +0000

    Autobuild

--- jeek.js
+++ jeek.js
@@ -2999,7 +2999,27 @@ function finalform(ns) {
 export class Infiltrations {
     doc = {
         'getPossibleLocations': 'stuff',
-        'getInfiltration': 'more stuff'
+        'getInfiltration': `
+# Infiltration.getInfiltration() method
+---
+Get all infiltrations with difficulty, location and rewards.
+
+Signature:
+
+\`getInfiltration(location: string): InfiltrationLocation;\`
+
+Parameters
+
+Parameter	Type	Description
+location	string	
+
+Returns:        
+InfiltrationLocation
+        
+Infiltration data for given location.
+        
+Remarks
+RAM cost: 15 GB`
     };
     constructor(ns, Game, settings = {}) {
         this.ns = ns;
@@ -3024,7 +3044,9 @@ export class Infiltrations {
             }
         })();
     }
-}export class Jeekipedia {
+}import { levenshteinDistance } from "helpers.js";
+
+export class Jeekipedia {
 	constructor(ns, Game) {
 		this.ns = ns;
 		this.Game = Game ? Game : new WholeGame(ns);
@@ -3032,16 +3054,50 @@ export class Infiltrations {
 	async lookup(functionName) {
 		this.ns.iKnowWhatImDoing();
 		let lookupData = functionName.split(".");
-		this.ns.tprint(lookupData);
+		lookupData[1] = (Object.keys(this.Game).map(x => [levenshteinDistance(x, lookupData[1]), x]).sort((a, b) => a[0] - b[0]))[0][1]
 		if (Object.keys(this.Game).includes(lookupData[1])) {
-            if (Object.keys(this.Game[lookupData[1]].includes("doc"))) {
-				eval('window').tprintRaw(this.render(this.Game[lookupData[1]].doc[lookupData[2]]));
+            if (Object.keys(this.Game[lookupData[1]]).includes("doc")) {
+				lookupData[2] = (Object.keys(this.Game[lookupData[1]].doc).map(x => [levenshteinDistance(x, lookupData[2]), x]).sort((a, b) => a[0] - b[0]))[0][1]
+				eval('window').tprintRaw(await this.render(this.Game[lookupData[1]]['doc'][lookupData[2]]));
+				this.ns.exit();
 				return;
 			}
 		}
+		this.ns.exit();
 	}
 	async render(text) {
-		return React.createElement("span", {}, text);
+		let result = [];
+		for (let line of text.split("\n")) {
+			let didSomething = false;
+			if (line.slice(0, 2) === "# ") {
+				result.push(React.createElement("h1", {}, line.slice(2)))
+				didSomething = true;
+			}
+			if (line.slice(0, 3) === "## ") {
+				result.push(React.createElement("h2", {}, line.slice(3)))
+				didSomething = true;
+			}
+			if (line.slice(0, 4) === "### ") {
+				result.push(React.createElement("h3", {}, line.slice(4)))
+				didSomething = true;
+			}
+			if (line === "---") {
+				result.push(React.createElement("hr", {}, ""));
+				didSomething = true;
+			}
+			if (line.slice(0, 2) === "> ") {
+				result.push(React.createElement("blockquote", {}, line.slice(2)))
+				didSomething = true;
+			}
+			if (line.slice(0, 1) === "`" && line.slice(line.length-2, 1) === "`") {
+				result.push(React.createElement("table", {border: 1}, React.createElement("tr", {}, React.createElement("td", {}, line.slice(1, line.length-2)))));
+				didSomething = true;
+			}
+			if (!didSomething) {
+				result.push(React.createElement("span", {}, line));
+			}
+		}
+		return React.createElement("span", {}, result);
 	}
 }
 /* Find the jobs array
