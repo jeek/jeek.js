f4f66aef9e15827ebe04bc342db263daccef352e
T.J. Eckman <jeek@jeek.net>
2023-03-27 10:00:03 +0000

    Autobuild

--- jeek.js
+++ jeek.js
@@ -1224,10 +1224,10 @@ export async function bn2(Game, settings={}) {
             let chances = {}
             Object.keys(othergangs).filter(x => othergangs[x].territory > 0).map(x => chances[x] = Game['Gang']['getChanceToWinClash'](x))
             for (let other of Object.keys(chances)) {
-                await (chances[other]);
+                chances[other] = await (chances[other]);
             }
             let total = Object.keys(othergangs).map(x => chances[x] * othergangs[x].territory).reduce((a, b) => a + b);
-            if ((total / (1 - (await (Game['Gang']['getGangInformation']())).territory) >= settings['clashTarget']) || (Object.keys(chances).every(x => chances[x] >= settings['clashTarget'])));
+            if ((total / (1 - (await (Game['Gang']['getGangInformation']())).territory) >= settings['clashTarget']) || (Object.keys(chances).every(x => chances[x] >= settings['clashTarget'])))
                 Game['Gang']['setTerritoryWarfare'](true);
 
         }
@@ -3580,6 +3580,51 @@ export class DebugStuff {
 			}
 		}
 	}
+
+
+	// Dev menu
+	/** @param {NS} ns *
+export async function main(ns) {
+    const orig = React.createElement;
+    const origState = React.useState;
+    let stateCalls = 0;
+    let resolve;
+    const wrapState = function (...args) {
+        stateCalls++;
+        const state = origState.call(this, ...args);
+        // The 2nd useState returns the page
+        if (stateCalls === 2) {
+            resolve(state);
+            React.useState = origState;
+        }
+        return state;
+    }
+    React.createElement = function (...args) {
+        const fn = args[0];
+        if (typeof fn === "function" &&
+            String(fn).includes("Trying to go to a page without the proper setup")) {
+            React.createElement = orig;
+            // Perform next-level hooking
+            const wrapped = function (...args_) {
+                React.useState = wrapState;
+                return fn.call(this, ...args_);
+            }
+            return orig.call(this, wrapped, ...args.slice(1));
+        }
+        return orig.call(this, ...args);
+    }
+    const resultP = Promise.race([
+        new Promise((res) => resolve = res),
+        ns.asleep(5000)])
+        .finally(() => {
+            React.createElement = orig;
+            React.useState = origState;
+        });
+    // Force a rerender
+    ns.ui.setTheme(ns.ui.getTheme());
+    const [state, setState] = await resultP;
+    setState(typeof state === "string" ? "Dev" : 8);
+}
 */
 }
 
@@ -3715,7 +3760,7 @@ function uniqueID(s, random = false) {
 
 // Writes a command to a file, runs it, and then returns the result
 export async function Do(ns, command, ...args) { //FFIGNORE
-	if (["ns.gang.setTerritoryWarfare", "ns.bladeburner.stopBladeburnerAction", "ns.bladeburner.setActionLevel", "ns.bladeburner.setActionAutolevel", "ns.singularity.hospitalize"].includes(command)) {
+	if (["ns.sleeve.setToIdle", "ns.gang.setTerritoryWarfare", "ns.bladeburner.stopBladeburnerAction", "ns.bladeburner.setActionLevel", "ns.bladeburner.setActionAutolevel", "ns.singularity.hospitalize"].includes(command)) {
 		return await DoVoid(ns, command, ...args); //FFIGNORE
 	}
 	let progname = "/temp/proc-" + uniqueID(command);
