0824a885d36ec0574fae734f18514db8bda00d04
T.J. Eckman <jeek@jeek.net>
2023-04-05 05:25:36 -0400

    First attempt to not blow all money on stocks.

--- bn8.js
+++ bn8.js
@@ -81,7 +81,7 @@ export async function bn8(Game) {
                         shares = Math.floor(shares * .99);
                     }
                     if (shares * (await Do(Game.ns, "ns.stock.getBidPrice", stock)) > 200000) {
-                        if (shares > 0) Game.StockMarket.log("Bought " + shares.toString() + " of " + stock);
+                        if (shares > 0 && ((await (Game.StockMarket.portfolioValue)) * 9 < (await (Game.money)))) Game.StockMarket.log("Bought " + shares.toString() + " of " + stock);
                     }
                 } else {
                     if (data[0] > 0) {
@@ -104,7 +104,7 @@ export async function bn8(Game) {
                         shares *= .99;
                     }
                     if (shares * (await Do(Game.ns, "ns.stock.getBidPrice", stock)) > 200000) {
-                        if (shares > 0) Game.StockMarket.log("Shorted " + shares.toString() + " of " + stock);
+                        if (shares > 0 && ((await (Game.StockMarket.portfolioValue)) * 9 < (await (Game.money)))) Game.StockMarket.log("Shorted " + shares.toString() + " of " + stock);
                     }
                 } else {
                     if (data[2] > 0) {
